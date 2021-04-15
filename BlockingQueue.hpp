e533cc158638a6e128106b2cc496da71d0ee05e0
Keane2019 <chenyi198938@gmail.com>
2021-04-15 19:10:28 +0800

    found something interesting

--- BlockingQueue.hpp
+++ BlockingQueue.hpp
@@ -12,6 +12,14 @@ public:
     explicit BlockingQueue()
     {}
 
+    //实际使用中发现void Put(T&& x)能完全替代此接口
+    /*void Put(const T& x)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        queue_.push_back(x);
+        cond_.notify_one();
+    } */
+
     void Put(T&& x)
     {
         std::lock_guard<std::mutex> lock(mutex_);
@@ -34,6 +42,10 @@ public:
         T front(std::move(queue_.front()));
         queue_.pop_front();
         return front;
+        //return std::move(front); 错误
+        //直接返回本地变量即可
+        //编译器会决定使用NRVO或者move语义来进行优化
+        //使用条件语句返回会抑制编译器使用NRVO
     }
 
     size_t Size()
